1acce69a9337523eba44e841a41825901483f901
Aman mavi <amanmavi@Amans-Mac-mini.local>
2025-05-16 17:21:15 +0530

    latest performance layout changed

--- app/components/home/LatestPerformance.tsx
+++ app/components/home/LatestPerformance.tsx
@@ -13,57 +13,62 @@ const PieSection = () => {
         >
             {/* performance screenshot section */}
           <div
-            className="bg-cardBgColor border border-customDarkGray rounded-2xl shadow-[0_0_25px_rgba(176,176,176,0.4)]  p-8 col-span-12"
+            className="col-span-12"
           >
-            {/* Chart Section */}
-            <div className="mb-6">
-            <h2 className="text-4xl text-customDarkGray md:text-6xl font-normal mb-6">
-        Latest Performance Snapshot 
-        </h2>
-              {/* Chart Placeholder replaced with table */}
-              <div className="overflow-x-auto">
-                <table className="min-w-[600px] w-full text-center border-separate border-spacing-0 bg-ctaColor rounded-lg shadow-lg">
-                  <caption className="text-customLightGray text-2xl font-semibold mb-2 bg-black rounded-lg py-2 caption-top text-center">Why Nifty <span className="text-ctaColor">Shloka</span>?</caption>
-                  <thead>
-                    <tr className="bg-ctaColor text-black">
-                      <th className="border border-black px-6 py-3 text-lg font-bold"></th>
-                      <th className="border border-black px-6 py-3 text-lg font-bold">Nifty Shloka</th>
-                      <th className="border border-black px-6 py-3 text-lg font-bold">NIFTY 50</th>
-                    </tr>
-                  </thead>
-                  <tbody className="text-black text-lg">
-                    <tr>
-                      <td className="border border-black px-6 py-3 font-medium text-left">Return(TWRR)</td>
-                      <td className="border border-black px-6 py-3 font-semibold">31.64%</td>
-                      <td className="border border-black px-6 py-3 font-semibold">23.99%</td>
-                    </tr>
-                    <tr>
-                      <td className="border border-black px-6 py-3 font-medium text-left">Risk(Max Drawdown)</td>
-                      <td className="border border-black px-6 py-3 font-semibold text-customRed">-7.94%</td>
-                      <td className="border border-black px-6 py-3 font-semibold text-customRed">-15.77%</td>
-                    </tr>
-                    <tr>
-                      <td className="border border-black px-6 py-3 font-medium text-left">Current Drawdown</td>
-                      <td className="border border-black px-6 py-3 font-semibold text-customRed">-0.92%</td>
-                      <td className="border border-black px-6 py-3 font-semibold text-customRed">-8.42%</td>
-                    </tr>
-                  </tbody>
-                </table>
-                <div className="flex flex-col md:flex-row md:justify-between text-xs text-white mt-2 px-2">
-                  <span className="text-customLightGray">*Launched in August 2023 (~ 21 months)</span>
-                  <span className="text-customLightGray">As of 11-May-25</span>
+            
+            
+            <div className="flex flex-col md:flex-row md:justify-between space-y-6 md:space-y-0">
+              {/* Chart Section */}
+              <div className="md:w-1/2 border-b pb-4">
+                {/* Chart Placeholder replaced with table */}
+                <div className="overflow-x-auto">
+                  <table className="min-w-[600px] w-full text-center border-separate border-spacing-0 bg-ctaColor rounded-lg shadow-lg">
+                    <caption className="text-customLightGray text-3xl font-normal mb-2 bg-black rounded-lg py-2 caption-top text-center">Why Nifty <span className="text-ctaColor">Shloka</span>?</caption>
+                    <thead>
+                      <tr className="bg-ctaColor text-black">
+                        <th className="border border-black px-6 py-3 text-lg font-bold"></th>
+                        <th className="border border-black px-6 py-3 text-lg font-bold">Nifty Shloka</th>
+                        <th className="border border-black px-6 py-3 text-lg font-bold">NIFTY 50</th>
+                      </tr>
+                    </thead>
+                    <tbody className="text-black text-lg">
+                      <tr>
+                        <td className="border border-black px-6 py-3 font-medium text-left">Return(TWRR)</td>
+                        <td className="border border-black px-6 py-3 font-semibold">31.64%</td>
+                        <td className="border border-black px-6 py-3 font-semibold">23.99%</td>
+                      </tr>
+                      <tr>
+                        <td className="border border-black px-6 py-3 font-medium text-left">Risk(Max Drawdown)</td>
+                        <td className="border border-black px-6 py-3 font-semibold text-customRed">-7.94%</td>
+                        <td className="border border-black px-6 py-3 font-semibold text-customRed">-15.77%</td>
+                      </tr>
+                      <tr>
+                        <td className="border border-black px-6 py-3 font-medium text-left">Current Drawdown</td>
+                        <td className="border border-black px-6 py-3 font-semibold text-customRed">-0.92%</td>
+                        <td className="border border-black px-6 py-3 font-semibold text-customRed">-8.42%</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <div className="flex flex-col md:flex-row md:justify-between text-xs text-white mt-2 px-2">
+                    <span className="text-customLightGray">*Launched in August 2023 (~ 21 months)</span>
+                    <span className="text-customLightGray">As of 11-May-25</span>
+                  </div>
                 </div>
               </div>
-            </div>
-            {/* Description Section */}
-            <div className="text-left">
-              <h3 className="text-xl md:text-2xl text-customDarkGray font-normal mt-6 mb-3">
-                Our Objective
-              </h3>
-              <ul className="text-base font-light md:text-lg text-customLightGray space-y-2 list-disc pl-4">
-                <li>Maximize risk adjusted returns</li>
-                <li>Even through market volatility</li>
-              </ul>
+              
+              {/* Description Section */}
+              <div className="text-left md:w-1/2  md:flex md:flex-col md:justify-start  items-start md:ml-32">
+              <h2 className="text-4xl text-customLightGray  font-medium mb-12 mt-6">
+              Latest Performance  
+            </h2>
+                <div className='text-left text-customLightGray text-xl md:text-2xl font-extralight leading-tight'>
+                  <p>
+                    Our Objective:<br/>
+                    Maximize risk adjusted returns<br/>
+                    Even through market volatility
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
           {/* Graph screenshot section */}
