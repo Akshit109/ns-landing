49ce205643850d3faea172467bddfef666c33c99
Aman mavi <amanmavi@Amans-Mac-mini.local>
2025-08-18 18:43:07 +0530

    data updated

--- app/components/home/LatestPerformance.tsx
+++ app/components/home/LatestPerformance.tsx
@@ -53,10 +53,10 @@ const PieSection = () => {
                               Return(TWRR)
                             </td>
                             <td className='border border-black px-4 md:px-6 py-3 md:py-4 lg:py-5 xl:py-6 font-semibold'>
-                              36.15%
+                              37.86%
                             </td>
                             <td className='border border-black px-4 md:px-6 py-3 md:py-4 lg:py-5 xl:py-6 font-semibold'>
-                              25.83%
+                              27.21%
                             </td>
                           </tr>
                           <tr>
@@ -75,10 +75,10 @@ const PieSection = () => {
                               Current Drawdown
                             </td>
                             <td className='border border-black px-4 md:px-6 py-3 md:py-4 lg:py-5 xl:py-6 font-semibold text-customRed'>
-                              -5.31%
+                              -4.11%
                             </td>
                             <td className='border border-black px-4 md:px-6 py-3 md:py-4 lg:py-5 xl:py-6 font-semibold text-customRed'>
-                          -7.07%
+                          -6.04%
                             </td>
                           </tr>
                         </tbody>
@@ -98,7 +98,7 @@ const PieSection = () => {
                               Nifty Shloka
                             </p>
                             <p className='text-black font-semibold text-xl'>
-                              36.15%
+                              37.86%
                             </p>
                           </div>
                           <div className='text-center'>
@@ -106,7 +106,7 @@ const PieSection = () => {
                               NIFTY 50
                             </p>
                             <p className='text-black font-semibold text-xl'>
-                              25.83%
+                              27.21%
                             </p>
                           </div>
                         </div>
@@ -148,7 +148,7 @@ const PieSection = () => {
                               Nifty Shloka
                             </p>
                             <p className='text-customRed font-semibold text-xl'>
-                              -5.31%
+                              -4.11%
                             </p>
                           </div>
                           <div className='text-center'>
@@ -156,7 +156,7 @@ const PieSection = () => {
                               NIFTY 50
                             </p>
                             <p className='text-customRed font-semibold text-xl'>
-                              -7.07%
+                              -6.04%
                             </p>
                           </div>
                         </div>
--- app/components/pie-chart/summaryData.ts
+++ app/components/pie-chart/summaryData.ts
@@ -10,50 +10,50 @@ export interface AssetData {
 export const summary: AssetData[] = [
   {
     assetClass: "EQUITY",
-    pnl: "2406424.10",
-    presentValue: "27646832.71",
-    totalInvested: "25240408.60"
+    pnl: "2890623.29",
+    presentValue: "28140220.59",
+    totalInvested: "25249597.29"
   },
   {
     assetClass: "DEBT",
-    pnl: "754804.05",
-    presentValue: "8401763.11",
+    pnl: "740971.86",
+    presentValue: "8387930.92",
     totalInvested: "7646959.06"
   },
   {
     assetClass: "GOLD",
-    pnl: "969201.06",
-    presentValue: "3069634.68",
+    pnl: "1079282.94",
+    presentValue: "3179716.56",
     totalInvested: "2100433.62"
   },
   {
     assetClass: "LIQUID",
-    pnl: "9350.66",
-    presentValue: "1845240.45",
-    totalInvested: "1835889.79"
-  },
-  {
-    assetClass: "CASH",
-    pnl: "0.00",
-    presentValue: "1023920.16",
-    totalInvested: "1023920.16"
+    pnl: "11151.10",
+    presentValue: "2147044.00",
+    totalInvested: "2135892.90"
   },
   {
     assetClass: "INViTs/REiTs",
-    pnl: "114928.33",
-    presentValue: "884072.48",
+    pnl: "127700.79",
+    presentValue: "896844.94",
     totalInvested: "769144.15"
   },
   {
     assetClass: "SILVER",
-    pnl: "147429.00",
-    presentValue: "660535.68",
+    pnl: "145658.28",
+    presentValue: "658764.96",
     totalInvested: "513106.68"
   },
   {
+    assetClass: "CASH",
+    pnl: "0.00",
+    presentValue: "616789.40",
+    totalInvested: "616789.40"
+  },
+  {
     assetClass: "HYBRID",
-    pnl: "-97.26",
-    presentValue: "13902.44",
+    pnl: "-60.99",
+    presentValue: "13938.71",
     totalInvested: "13999.69"
   }
 ]; 
