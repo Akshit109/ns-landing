6af2b052654980359ce2cdfd3d0cff69722ec0bc
Aman mavi <amanmavi@Amans-Mac-mini.local>
2025-11-03 16:14:22 +0530

    data updated

--- app/components/home/LatestPerformance.tsx
+++ app/components/home/LatestPerformance.tsx
@@ -56,10 +56,10 @@ const PieSection = () => {
                               Return (TWRR Annualised)
                             </td>
                             <td className='border-l border-black px-2 md:px-4 lg:px-6 py-3 md:py-4 lg:py-5 xl:py-6 font-semibold text-green-600'>
-                              17.13%
+                              16.41%
                             </td>
                             <td className='border-l border-black px-2 md:px-4 lg:px-6 py-3 md:py-4 lg:py-5 xl:py-6 font-semibold text-green-600'>
-                              12.56%
+                              14.07%
                             </td>
                             <td className='border-l border-r border-black px-2 md:px-4 lg:px-6 py-3 md:py-4 lg:py-5 xl:py-6 font-semibold text-green-600'>
                               36%
@@ -96,7 +96,7 @@ const PieSection = () => {
                               Nifty Shloka
                             </p>
                             <p className='text-green-600 font-semibold text-lg'>
-                              17.13%
+                              16.41%
                             </p>
                           </div>
                           <div className='text-center'>
@@ -104,7 +104,7 @@ const PieSection = () => {
                               NIFTY 50
                             </p>
                             <p className='text-green-600 font-semibold text-lg'>
-                              12.56%
+                              14.07%
                             </p>
                           </div>
                           <div className='text-center'>
@@ -155,10 +155,10 @@ const PieSection = () => {
                     <div className='flex flex-col md:flex-row md:justify-between text-xs text-white mt-2 px-2'>
                       <div className='text-customLightGray space-y-1 md:space-y-0 md:flex md:flex-col md:items-start'>
                         <div className='text-left'>*Zerodha verified</div>
-                        <div className='text-left'>*Launched in August 2023 (~ 26 months)</div>
+                        <div className='text-left'>*Launched in August 2023 (~ 28 months)</div>
                       </div>
                       <span className='text-customLightGray mt-2 md:mt-0'>
-                        As of 30-Sep-25
+                        As of 31-Oct-25
                       </span>
                     </div>
                   </div>
--- app/components/pie-chart/summaryData.ts
+++ app/components/pie-chart/summaryData.ts
@@ -10,50 +10,50 @@ export interface AssetData {
 export const summary: AssetData[] = [
   {
     assetClass: "EQUITY",
-    pnl: "3723328.82",
-    presentValue: "28820810.75",
-    totalInvested: "25097481.93"
+    pnl: "4787342.34",
+    presentValue: "30300669.68",
+    totalInvested: "25513327.34"
   },
   {
     assetClass: "DEBT",
-    pnl: "783482.04",
-    presentValue: "8690424.87",
-    totalInvested: "7906942.83"
+    pnl: "798750.18",
+    presentValue: "8624812.37",
+    totalInvested: "7826062.19"
   },
   {
     assetClass: "GOLD",
-    pnl: "1735238.64",
-    presentValue: "3890800.25",
-    totalInvested: "2155561.61"
+    pnl: "1573266.35",
+    presentValue: "3789467.96",
+    totalInvested: "2216201.61"
   },
   {
     assetClass: "LIQUID",
-    pnl: "11977.71",
-    presentValue: "2307221.00",
-    totalInvested: "2295243.29"
+    pnl: "2675.38",
+    presentValue: "2014919.15",
+    totalInvested: "2012243.77"
   },
   {
     assetClass: "INViTs/REiTs",
-    pnl: "176759.59",
-    presentValue: "961036.80",
-    totalInvested: "784277.21"
+    pnl: "197322.65",
+    presentValue: "1096561.56",
+    totalInvested: "899238.91"
   },
   {
     assetClass: "CASH",
     pnl: "0.00",
-    presentValue: "1221448.41",
-    totalInvested: "1221448.41"
+    presentValue: "800794.32",
+    totalInvested: "800794.32"
   },
   {
     assetClass: "SILVER",
-    pnl: "279839.95",
-    presentValue: "809073.60",
-    totalInvested: "529233.65"
+    pnl: "338150.57",
+    presentValue: "875435.82",
+    totalInvested: "537285.25"
   },
   {
     assetClass: "HYBRID",
-    pnl: "519.89",
-    presentValue: "27519.17",
-    totalInvested: "26999.28"
+    pnl: "1688.88",
+    presentValue: "47687.05",
+    totalInvested: "45998.17"
   }
 ]; 
